79bfd7f20683d0c53af916ea35cc9b5e811733ea
julieta <julieta.lopez@darwoft.com>
2024-02-02 12:24:00 -0300

    Add failing and skipped tests

--- cypress/e2e/prueba.cy.js
+++ cypress/e2e/prueba.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Test de prueba para Jenkins', () => {
+  it('test que PASA', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('test que FALLA', () => {
+    cy.visit('https://example.cypress.io')
+    cy.get("h1").should("have.text", "Hola Mundo!")
+  })
+
+  it.skip('test que NO SE EJECUTA', () => {
     cy.visit('https://example.cypress.io')
   })
 })
